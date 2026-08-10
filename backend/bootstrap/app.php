3f928d5355933e2c0cf253be2943254f0368562d
dkkhare <devendra.kkhare@gmail.com>
2026-08-10 12:49:06 +0530

    Register tenant administrator middleware

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 use App\Http\Middleware\EnsureSuperAdministrator;
+use App\Http\Middleware\EnsureTenantAdministrator;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -7,8 +8,7 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(api: __DIR__.'/../routes/api.php', commands: __DIR__.'/../routes/console.php', health: '/up')
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->alias(['super_admin' => EnsureSuperAdministrator::class]);
+        $middleware->alias(['super_admin' => EnsureSuperAdministrator::class, 'tenant_admin' => EnsureTenantAdministrator::class]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {})
     ->create();
-
